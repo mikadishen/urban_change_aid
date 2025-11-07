b774917ccaded8fdb22e97c93d611b7308be5b19
Geo Smart <125778240+mikadishen@users.noreply.github.com>
2025-11-07 11:24:15 -0300

    update _init_

--- __init__.py
+++ __init__.py
@@ -1,13 +1,31 @@
+# -*- coding: utf-8 -*-
 """
-Urban Change Detection Aid Plugin for QGIS
+/***************************************************************************
+ Urban Change AID
+                                 A QGIS Plugin
+ Análise e detecção de mudanças urbanas assistida por IA
 
-This plugin provides tools for detecting urban changes between two time periods
-using satellite or aerial imagery.
+                              -------------------
+        begin                : 2025-11-07
+        copyright            : (C) 2025 by Leandro da Silva Gregorio
+        email                : 
+        license              : GPL v3
+ ***************************************************************************/
 
-Author: [Leandro Gregorio]
-Date: [Current Date]
-Version: 1.0
+Este programa é software livre: você pode redistribuí-lo e/ou modificá-lo
+sob os termos da Licença Pública Geral GNU conforme publicada pela Free
+Software Foundation, tanto a versão 3 da Licença como (a seu critério)
+qualquer versão posterior.
+
+Este programa é distribuído na esperança de que seja útil, mas SEM
+QUALQUER GARANTIA; sem mesmo a garantia implícita de COMERCIALIZAÇÃO ou
+ADEQUAÇÃO A UM DETERMINADO PROPÓSITO. Consulte a Licença Pública Geral GNU
+para mais detalhes.
+
+Você deve ter recebido uma cópia da Licença Pública Geral GNU
+junto com este programa. Caso contrário, veja <https://www.gnu.org/licenses/>.
 """
+
 
 def classFactory(iface):
     """
